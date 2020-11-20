0d7e5ed3d145cd2b75023e75890328a754c0f974
William Horn <47996337+WJHorn@users.noreply.github.com>
2020-11-19 19:15:55 -0500

    add working database for macros
    next step is calculating macros with javascript. First I need to figure out how to give java access to to the SQL table's values

--- macros.php
+++ macros.php
@@ -11,7 +11,7 @@
    <div class="menu">
       <nav>
          <li><a href="prj.html"> HOME </a></li>
-			<li><a href="macros.html"> CALCULATE MACROS </a></li>
+			<li><a href="macros.php"> CALCULATE MACROS </a></li>
 			<li><a> OTHER </a></li>			
 		</nav>
 	</div>
@@ -29,16 +29,94 @@
                <th> Weight: </th>
             </tr>
             <tr>
-               <td><input type = "text" name = "age" size = "3" placeholder = "24" ></td>
-               <td><input type = "text" name = "gender" size = "3" placeholder = "M" ></td>
-               <td><input type = "text" name = "height" size = "3" placeholder = "6.0" ></td>
-               <td><input type = "text" name = "weight" size = "3" placeholder = "200" ></td>
+               <td><input type = "text" name = "age" size = "3" value = "24"></td>
+               <td>
+                  <p>
+                  <input type = "radio" name = "gender" value = "male" checked = "checked" >M
+                  <input type = "radio" name = "gender" value = "female" >F
+                  </p>
+               </td>
+               <td><input type = "text" name = "height" size = "3" value = "6.0" ></td>
+               <td><input type = "text" name = "weight" size = "3" value = "200" ></td>
             </tr>
             <tr>
-               <td colspan = "4" align = "center"><input type = "submit" value = "Calculate my Macros"></td>
+               <td colspan = "4" align = "center"><input type = "submit" name = "macros" value = "Calculate my Macros"></td>
             </tr>
          </table>
       </div>
    </form>
+      <?php
+      //get input data
+         if(isset($_POST['macros'])) {
+            $age = $_POST['age'];
+            if ($_POST['gender'] == "male") {
+               $gender = "M";
+            }
+            else if ($_POST['gender'] == "female") {
+               $gender = "F";
+            }
+            else {
+               $gender = "N";
+            }
+            $height = $_POST['height'];
+            $weight = $_POST['weight'] ;
+         }
+         else {
+            $age = 0;
+            $gender = 'N';
+            $height = '0.0';
+            $weight = 0;
+            $query = "";
+            
+         }
+         $db = mysqli_connect("localhost", "root", "");
+         if (!$db) {
+            print "Error - Could not connect to MySQL";
+            exit;
+         }
+         
+         $er = mysqli_select_db($db, "keto_tracker");
+         if(!$er) {
+            print "Error - Could not select the database";
+            exit;
+         }
+         
+         if(strcmp($gender,'M') == 0) {
+            echo "<div id = 'gender'>Male</div>";
+            $query = "insert into macroVars(Age, Gender, Height, Weight) values('$age', '$gender', '$height', '$weight')";
+         }
+         else if (strcmp($gender,'F') == 0) {
+            echo "<div id = 'gender'>Female</div>";
+            $query = "insert into macroVars(Age, Gender, Height, Weight) values('$age', '$gender', '$height', '$weight')";
+         }
+         else {
+            echo "<div id = 'gender'>n/a</div>";
+         }
+         
+         
+         
+         if ($query != "") {
+            $result = mysqli_query($db, $query);
+            if (!$result) {
+               print "Error - the query could not be executed";
+               $error = mysqli_error($db);
+               print "<p>" . $error . "</p>";
+            }
+         }
+      ?>
+      <script type = "text/javascript">
+         if (document.getElementById("gender") == 'Male'){
+            maleCalc();
+         }
+         else if (){
+            
+         }
+         function maleCalc(){
+            <?php echo "Male Funtion"?>
+         }
+         function femaleCalc(){
+            
+         }
+      </script>
 </body>
 </html>
